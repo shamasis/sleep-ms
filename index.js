89921d2b45c9b61996710b2c698ec429c62c2487
Adrian Miranda <adriancmiranda@gmail.com>
2016-09-06 12:45:44 -0300

    Update index.js

--- index.js
+++ index.js
@@ -2,11 +2,13 @@
  * This function halts the execution of a function by a specific duration
  * @param  {number} ms Time in milliseconds
  */
+ 
+var noop = function(){};
 module.exports = function (ms) {
 	// validate parameter and typecast ms to number so that loop checking does not burden CPU
 	if (isNaN(ms) || ms < 0) { return; }
 	ms = Number(ms);
 
 	// do setimeout for the specified interval
-	setTimeout(function(){}, ms);
+	setTimeout(noop, ms);
 };
