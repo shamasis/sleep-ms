3f3b19a160e700a08636b7cfdd3e509ee90cbaf4
Adrian Miranda <adriancmiranda@gmail.com>
2016-09-06 12:48:08 -0300

    Update index.js

--- index.js
+++ index.js
@@ -1,9 +1,12 @@
 /**
+ * Computer protocol command that does nothing
+ */
+var noop = function(){};
+
+/**
  * This function halts the execution of a function by a specific duration
  * @param  {number} ms Time in milliseconds
  */
- 
-var noop = function(){};
 module.exports = function (ms) {
 	// validate parameter and typecast ms to number so that loop checking does not burden CPU
 	if (isNaN(ms) || ms < 0) { return; }
